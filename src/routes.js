b3f534569debb8a6f29c15067630bdbbcc889f82
Albreis - Design & Programação <albreisdev@gmail.com>
2020-01-28 16:51:26 -0200

    Alterado modo de rotas para "history"

--- src/routes.js
+++ src/routes.js
@@ -6,15 +6,16 @@ import Categoria from './pages/Categoria.vue';
 Vue.use(Router);
 
 const routes = [
-  { path: '/', component: Agenda },
-  { path: '/:slug', component: Categoria }
+  { path: '/', component: Agenda},
+  { path: '/:slug', component: Categoria, params: ['slug', 'name', 'id'] }
 ]
 
 // 3. Create the router instance and pass the `routes` option
 // You can pass in additional options here, but let's
 // keep it simple for now.
 const router = new Router({
-  routes // short for `routes: routes`
+  mode: 'history',
+  routes: routes // short for `routes: routes`
 })
 
 export default router
